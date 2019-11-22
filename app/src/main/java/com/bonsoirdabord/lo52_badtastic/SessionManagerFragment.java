dd3dd9146c1a7d1c0304f6c0021814f2eba2618f
amau68320 <amaury.einholtz@utbm.fr>
2019-11-22 15:40:41 +0100

    -à tester : layout dynamique avec items

---
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/SessionManagerFragment.java
@@ -0,0 +1,18 @@
+package com.bonsoirdabord.lo52_badtastic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SessionManagerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_session_manager,
+                container, false);
+        return view;
+    }
+}
