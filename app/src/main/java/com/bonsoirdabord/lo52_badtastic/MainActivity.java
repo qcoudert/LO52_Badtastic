b63bf42a2371b6e756ff88a8c8cf30c89634057a
Nicolas BARBOTIN <nicolas@barbot.in>
2019-11-22 16:06:37 +0100

    + Added caldroid widget to scheduler activity

--- app/src/main/java/com/bonsoirdabord/lo52_badtastic/MainActivity.java
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/MainActivity.java
@@ -2,7 +2,9 @@ package com.bonsoirdabord.lo52_badtastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startSchedulerActivity(View v) {
+        startActivity(new Intent(this, CalendarActivity.class));
+    }
+
 }
