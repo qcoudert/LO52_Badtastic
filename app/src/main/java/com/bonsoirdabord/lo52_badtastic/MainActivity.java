803c95bef071e7bbb7a89450d41a5eb8de56baee
amau68320 <amaury.einholtz@utbm.fr>
2019-12-06 09:35:42 +0100

    changement images button

--- app/src/main/java/com/bonsoirdabord/lo52_badtastic/MainActivity.java
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/MainActivity.java
@@ -2,6 +2,7 @@ package com.bonsoirdabord.lo52_badtastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
