b02d464104a42802d7629462ec7443632f47d77b
Julien <julien.medici.78@gmail.com>
2020-01-09 17:57:59 +0100

    Commit temp SessionGenerator

--- app/src/main/java/com/bonsoirdabord/lo52_badtastic/SessionGenerator.java
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/SessionGenerator.java
@@ -1,14 +1,21 @@
 package com.bonsoirdabord.lo52_badtastic;
 
+import android.content.Context;
+
+import com.bonsoirdabord.lo52_badtastic.beans.Exercise;
 import com.bonsoirdabord.lo52_badtastic.beans.Session;
+import com.bonsoirdabord.lo52_badtastic.database.ExerciseDatabase;
+
+import java.util.List;
 
 public class SessionGenerator {
 
     /***
      *  Get a Session with GroupTraining list and generate the exercise sets with parameter of the GroupTrainings.
-     * @param session with GroupTraining
+     * @param context : Context of the app for database
+     * @param session : Session with GroupTraining
      */
-    public void generateSession(Session session){
-
+    public void generateSession(Context context, Session session){
+        List<Exercise> exercises = ExerciseDatabase.getInstance(context).exerciseDAO().getExerciseCompleted();
     }
 }
