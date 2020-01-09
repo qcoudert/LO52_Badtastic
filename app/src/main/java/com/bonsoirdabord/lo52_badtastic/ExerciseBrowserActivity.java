c087cbedee603010457cf52715b0f04a5ef83e48
Nicolas BARBOTIN <nicolas@barbot.in>
2020-01-09 22:28:53 +0100

    * Preparing merge into master

---
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/ExerciseBrowserActivity.java
@@ -0,0 +1,14 @@
+package com.bonsoirdabord.lo52_badtastic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExerciseBrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_browser);
+    }
+}
