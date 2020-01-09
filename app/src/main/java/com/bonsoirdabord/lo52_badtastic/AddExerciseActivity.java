06bb5aa28db57b686e1a28d82df16c41127824d5
qcoudert <quentin.coudert@utbm.fr>
2020-01-09 13:38:26 +0100

    +Push avant merge depuis master

--- app/src/main/java/com/bonsoirdabord/lo52_badtastic/AddExerciseActivity.java
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/AddExerciseActivity.java
@@ -1,8 +1,18 @@
 package com.bonsoirdabord.lo52_badtastic;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.style.ImageSpan;
+import android.widget.EditText;
+
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddExerciseActivity extends AppCompatActivity {
 
@@ -10,5 +20,35 @@ public class AddExerciseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+
+        Toolbar tbar = (Toolbar) findViewById(R.id.toolbar_add_ex);
+        setSupportActionBar(tbar);
+
+        TextInputEditText tiet = (TextInputEditText) findViewById(R.id.edit_text_tags_add_ex);
+        tiet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                for(int is = i1; is<=i2; i++) {
+                    if(charSequence.charAt(is)==' ') {
+                        ChipDrawable chip = ChipDrawable.createFromResource(getContext(), R.xml.chip);
+                        chip.setChipText(editable.subSequence(SpannedLength,editable.length()));
+                        chip.setBounds(0, 0, chip.getIntrinsicWidth(), chip.getIntrinsicHeight());
+                        ImageSpan span = new ImageSpan(chip);
+                        editable.setSpan(span, SpannedLength, editable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        SpannedLength = editable.length();
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
