fe9a26837208b461fb9a3c16a39c1884456e2d30
Julien <julien.medici.78@gmail.com>
2020-01-09 17:33:31 +0100

    Create SessionGenerator

---
+++ app/src/main/java/com/bonsoirdabord/lo52_badtastic/SessionGenerator.java
@@ -0,0 +1,14 @@
+package com.bonsoirdabord.lo52_badtastic;
+
+import com.bonsoirdabord.lo52_badtastic.beans.Session;
+
+public class SessionGenerator {
+
+    /***
+     *  Get a Session with GroupTraining list and generate the exercise sets with parameter of the GroupTrainings.
+     * @param session with GroupTraining
+     */
+    public void generateSession(Session session){
+
+    }
+}
